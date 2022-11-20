6ab5c7da4f8f47e116b6a2b1ef413f78be2b5b81
faker <N18989364491@163.com>
2022-11-20 14:56:34 +0800

    后台创建，并配置数据库

---
+++ Springboot/src/main/java/com/qingqu/boot/QingquApplication.java
@@ -0,0 +1,13 @@
+package com.qingqu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingquApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingquApplication.class, args);
+	}
+
+}
---
+++ Springboot/src/test/java/com/qingqu/boot/QingquApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingqu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingquApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
